19cd953f9ad5287d708b53a6235423698cbd092a
Pasha Majied <42023756+pashamajied@users.noreply.github.com>
2022-10-15 10:57:44 +0700

    Update rsync-ssh-password.sh

--- rsync-ssh-password.sh
+++ rsync-ssh-password.sh
@@ -48,7 +48,7 @@ TEXT="Hostname = $hostname [$TIME] %0ABackup folder = $REMOTE_PATH %0APada Serve
 
 sshpass -p $REMOTE_PASSWORD /usr/bin/rsync -rsh -e ssh $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH $LOCAL_PATH/$YEAR/$MONTH/
 sleep 1
-curl -s --data "text=$TEXT" --data "chat_id=$GROUP_ID" 'https://api.telegram.org/bot'$BOT_TOKEN'/sendMessage'
+curl -s --data "text=$TEXT" --data "chat_id=$CHAT_ID" 'https://api.telegram.org/bot'$BOT_TOKEN'/sendMessage'
 
 tar -cvf $LOCAL_PATH/$YEAR/$MONTH/wwwroot-$DATETIME.tar.gz $LOCAL_PATH/$YEAR/$MONTH/
 sleep 1
